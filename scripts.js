c85dc5bd4635e3ccf4842657badfd4a6afe2a17d
jgplack13 <116857597+jgplack13@users.noreply.github.com>
2022-11-01 01:16:00 -0300

    commit

---
+++ scripts.js
@@ -0,0 +1,15 @@
+const btnMobile = document.getElementById('btn-mobile');
+btn.addEventListener('click', toggleMenu());
+
+function Menu() {
+    let menu = document.querySelector('#menu');
+    const nav = document.getElementById('nav');
+    nav.classList.toggle('active');
+}
+
+
+
+
+
+
+
